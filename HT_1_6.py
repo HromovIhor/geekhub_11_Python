6a9f8ca6ec978daf85c50c6a36171a7a11fd1bc8
Ihor Hromov <94138568+HromovIhor@users.noreply.github.com>
2021-11-18 14:54:39 +0200

    Delete HT_1_6.py

--- HT_1_6.py
+++
@@ -1,7 +0,0 @@
-list_of_numbers = list(input("Specify the comma separated numbers: ").split(", "))
-check_up_number = input("Specify the check-up number: ")
-for i in list_of_numbers:
-    if check_up_number in i:
-        print("True")
-    else:
-        print("False")
