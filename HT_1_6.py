18b6fef042f4f1fbca4bf5b13129f0d09ff6629e
Ihor Hromov <94138568+HromovIhor@users.noreply.github.com>
2021-11-18 14:48:36 +0200

    Update HT_1_6.py

--- HT_1_6.py
+++ HT_1_6.py
@@ -1,4 +1,7 @@
-def is_in_list(list, number):
-   return number in list
-print(is_in_list([1, 5, 8, 3], 3))
-print(is_in_list([5, 8, 3], -1))
+list_of_numbers = list(input("Specify the comma separated numbers: ").split(", "))
+check_up_number = input("Specify the check-up number: ")
+for i in list_of_numbers:
+    if check_up_number in i:
+        print("True")
+    else:
+        print("False")
