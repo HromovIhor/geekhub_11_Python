89947764533aed1a5dd82514602ecdda5804075a
Ihor Hromov <94138568+HromovIhor@users.noreply.github.com>
2021-11-18 14:27:58 +0200

    Update HT_1_5.py

--- HT_1_5.py
+++ HT_1_5.py
@@ -1,2 +1,4 @@
 decimal_number = int(input("Enter any decimal number: "))
-print("Hexadecimal number will be: ",hex(decimal_number))
+hex_number = hex(decimal_number)[2:]
+#print(hex(decimal_number))
+print("Hexadecimal number will be: ",hex_number)
