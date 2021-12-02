ec108422353aa142c6efd12b882ea7003e8c192b
Ihor Hromov <94138568+HromovIhor@users.noreply.github.com>
2021-12-02 12:29:16 +0200

    Update HT_5_6.py

--- HT_5/HT_5_6.py
+++ HT_5/HT_5_6.py
@@ -14,3 +14,9 @@ def range_analog(start=0,stop=0,step=1):
 
 test = [n for n in range_analog(10)]
 print(test)
+
+test_1 = [n for n in range_analog(5,26)]
+print(test_1)
+
+test_2 = [n for n in range_analog(5,26,2)]
+print(test_2)
