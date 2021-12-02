fd4cf9328f9e9b0c4a2ed1f6f6f98da0628cccdb
Ihor Hromov <94138568+HromovIhor@users.noreply.github.com>
2021-12-02 12:23:50 +0200

    Update HT_5_6.py

--- HT_5/HT_5_6.py
+++ HT_5/HT_5_6.py
@@ -3,11 +3,14 @@
 P.S. Повинен вертатись генератор.
 """
 
-def range_analog(start,stop,step=1):
+def range_analog(start=0,stop=0,step=1):
+    if stop == 0:
+        stop = start
+        start = 0
     while start < stop:
         yield start
         start += step
     return
 
-test = [n for n in range_analog(0,10)]
+test = [n for n in range_analog(10)]
 print(test)
